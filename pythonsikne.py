188094ce14796823abb777906f996a94e4e088d3
Utkrishta Bhattarai <bhattarai.utk1@gmail.com>
2020-07-28 22:42:32 +0545

    first commit

---
+++ pythonsikne.py
@@ -0,0 +1,36 @@
+
+a="1234"
+print(a)
+
+#lists also known as array in some language
+my_dogs=["lucy","lucly"]
+print(type(my_dogs))
+
+#dictionary first one is key and second one is value
+dictionary={"name":"ram","age":"12"}
+print(type(dictionary))
+
+#float value also known as decimal 
+salary=10021.21
+print(type(salary))
+
+#string value
+name="RAM"
+print(type(name))
+
+#integer 
+quantity=2
+print(type(quantity))
+
+#tuples collection of immutable sequence of object
+collection=("RAM",10,199.99)
+print(type(collection))
+
+
+#sets unorder collection
+union={"a","c"}
+print(type(union))
+
+# boolen true of false
+is_covidpatient=True
+print(type(is_covidpatient))
